ce090ea6a1f2a66583b093894ede7436c231ce90
Nathan Vogel <nathan@nathanvogel.com>
2018-08-27 22:45:33 +0200

    Initial Commit

---
+++ NVMultiProgressView/NVMultiProgressView.h
@@ -0,0 +1,19 @@
+//
+//  NVMultiProgressView.h
+//  NVMultiProgressView
+//
+//  Created by Nathan Vogel +41786097927 on 27.08.18.
+//  Copyright © 2018 Blended Study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVMultiProgressView.
+FOUNDATION_EXPORT double NVMultiProgressViewVersionNumber;
+
+//! Project version string for NVMultiProgressView.
+FOUNDATION_EXPORT const unsigned char NVMultiProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVMultiProgressView/PublicHeader.h>
+
+
